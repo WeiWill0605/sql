a200fe84f1b4261cd07abd3c5d9fcee749616b8b
Will.Wei <577452271@qq.com>
2019-10-25 16:27:51 +0800

    'backup'

--- pri_zwei/ca_cannabis_store_pri.sql
+++ pri_zwei/ca_cannabis_store_pri.sql
@@ -1,5 +1,6 @@
 SELECT * FROM great_wall.ca_cannabis_store order by 1 desc;
-SELECT * FROM great_wall.ca_cannabis_store where province = 'british_columbia'  order by 1 desc;
+SELECT * FROM great_wall.ca_cannabis_store where province = 'nova_scotia'  order by 1 desc;
 SELECT * FROM great_wall.ca_cannabis_store where rundate = (select max(rundate) from great_wall.ca_cannabis_store  );
  
 SELECT count(1), rundate FROM great_wall.ca_cannabis_store group by rundate desc ;
+SELECT count(1), rundate FROM great_wall.ca_cannabis_store where province = 'nova_scotia'  group by rundate desc ;
